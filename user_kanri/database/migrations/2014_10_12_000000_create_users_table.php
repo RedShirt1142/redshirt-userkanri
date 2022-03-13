725ad0314b42a159280fe942b4f9d5eea5ebf529
RedShirt1142 <red.perc.shirt@icloud.com>
2022-03-13 12:30:53 +0900

    auto deploy

--- user_kanri/database/migrations/2014_10_12_000000_create_users_table.php
+++ user_kanri/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->integer('phone')->unsigned();
-            $table->string('email', 254);
+            $table->string('email', 254)->charset("utf8");
             $table->timestamps();
         });
     }
